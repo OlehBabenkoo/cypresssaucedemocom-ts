1ad7d9da28d7536c3444e10a047c118759ac4d54
Oleh Babenko <olegbabenko12@ukr.net>
2022-07-14 23:03:36 +0300

    Update navigations.cy.ts

--- cypress/e2e/navigations.cy.ts
+++ cypress/e2e/navigations.cy.ts
@@ -1,4 +1,4 @@
-import LoginPage from "../page-object/pages/loginPage";
+import LoginPage from "../page-object/pages/LoginPage";
 const loginPage = new LoginPage();
 
 describe('Navigations test', () => {

b92fb62a30d686a71994824547fb4ecf665ac6e1
Oleh Babenko <olegbabenko12@ukr.net>
2022-08-01 16:36:04 +0300

    Update InventoryPage.ts

--- cypress/page-object/pages/InventoryPage.ts
+++ cypress/page-object/pages/InventoryPage.ts
@@ -7,7 +7,10 @@ export default class InventoryPage extends BasePage {
     public header: Header = new Header();
     public footer: Footer = new Footer();
     private inventoryContainer: string = '#inventory_container';
+    private inventoryItemTitleLocator: string = '.inventory_item_name';
+    private inventoryItemDescLocator: string = '.inventory_item_desc';
     private inventoryItemPriceLocator: string = '.inventory_item_price';
+    private inventoryItemButtonLocator: string = '[data-test^="add-to-cart"]';
     private inventoryTitleListLocator: string = '[id$="title_link"]';
 
     constructor() {
@@ -17,46 +20,20 @@ export default class InventoryPage extends BasePage {
     private get inventoryItems(): Cypress.Chainable {
         return cy.get(`${this.inventoryContainer} [class="inventory_item"]`);
     }
-    private get inventoryItemTitle(): Cypress.Chainable {
-        return cy.get(`${this.inventoryContainer} [class="inventory_item_name"]`);
-    }
-    private get inventoryItemDescription(): Cypress.Chainable {
-        return cy.get(`${this.inventoryContainer} [class="inventory_item_desc"]`);
-    }
-    private get inventoryItemPrice(): Cypress.Chainable {
-        return cy.get(`${this.inventoryContainer} [class="inventory_item_price"`);
-    }
-    private get inventoryItemButton(): Cypress.Chainable {
-        return cy.get(`${this.inventoryContainer} [class="btn btn_primary btn_small btn_inventory"]`);
-    }
     private getArrayOfItemsPrice(): Cypress.Chainable {
         return this.inventoryItems.find(this.inventoryItemPriceLocator).then(($inventoryItemPrice) => {
             return Cypress._.map(Cypress.$.makeArray($inventoryItemPrice), 'innerText').map(price => price.replace('$', ''));
         });
     }
-    private checkItemsTitle(): this {
-        this.inventoryItemTitle.should('be.visible');
-        return this;
-    }
-    private checkItemsDescription(): this {
-        this.inventoryItemDescription.should('be.visible');
-        return this;
-    }
-    private checkItemsPrice(): this {
-        this.inventoryItemPrice.should('be.visible');
-        return this;
-    }
-    private checkItemsButton(): this {
-        this.inventoryItemButton.should('be.visible');
-        return this;
-    }
-    public checkProductsField(): this {
-        this.checkItemsTitle()
-            .checkItemsDescription()
-            .checkItemsPrice()
-            .checkItemsButton();
-        return this;
 
+    public checkProductsField(): this {
+        this.inventoryItems.each(inventoryItem => {
+            cy.wrap(inventoryItem).find(this.inventoryItemTitleLocator).should('be.visible');
+            cy.wrap(inventoryItem).find(this.inventoryItemDescLocator).should('be.visible');
+            cy.wrap(inventoryItem).find(this.inventoryItemPriceLocator).should('be.visible');
+            cy.wrap(inventoryItem).find(this.inventoryItemButtonLocator).should('be.visible');
+        });
+        return this;
     }
     public checkGoodsIsSortedByLowToHi(): this {
         this.getArrayOfItemsPrice().each((currentItemPrice, index, list) => {
@@ -73,9 +50,9 @@ export default class InventoryPage extends BasePage {
     public clickOnRandomProduct(): InventoryItemPage {
         this.inventoryItems.then(inventoryItems => {
             cy.wrap(inventoryItems)
-            .eq(Cypress._.random(inventoryItems.length - 1))
-            .find(this.inventoryTitleListLocator)
-            .click();
+                .eq(Cypress._.random(inventoryItems.length - 1))
+                .find(this.inventoryTitleListLocator)
+                .click();
         });
         return new InventoryItemPage();
     }
